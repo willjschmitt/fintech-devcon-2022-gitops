2f71c0543953f94b1dce19664b112330b529dbd6
Will Schmitt <will@rightfoot.com>
2022-08-24 10:45:12 -0600

    Support specifying person to greet.

--- greeter-server/app.py
+++ greeter-server/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def index():
-    """Returns a simple hello world message."""
-    return jsonify({'message': 'Hello, world.'})
+    """Returns a simple hello world message, greeting the requested greetee."""
+    greetee = request.json['greetee']
+    return jsonify({'message': f'Hello, {greetee}.'})
 
 
 app.run()
